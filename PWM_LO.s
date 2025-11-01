2b25b584912faa7dd043ec9ecd939b5922b6d3a1
Darin J. Ulness <120402820+ulnessd@users.noreply.github.com>
2025-11-01 11:10:15 -0500

    Create PWM_LO.s
    Basic PWM program

---
+++ PWM_LO.s
@@ -0,0 +1,71 @@
+; PIC12F1822 Configuration Bit Settings
+
+; Assembly source line config statements
+
+; CONFIG1
+  CONFIG  FOSC = INTOSC         ; Oscillator Selection (INTOSC oscillator: I/O function on CLKIN pin)
+  CONFIG  WDTE = OFF            ; Watchdog Timer Enable (WDT disabled)
+  CONFIG  PWRTE = OFF           ; Power-up Timer Enable (PWRT disabled)
+  CONFIG  MCLRE = OFF            ; MCLR Pin Function Select (MCLR/VPP pin function is MCLR)
+  CONFIG  CP = OFF              ; Flash Program Memory Code Protection (Program memory code protection is disabled)
+  CONFIG  CPD = OFF             ; Data Memory Code Protection (Data memory code protection is disabled)
+  CONFIG  BOREN = ON            ; Brown-out Reset Enable (Brown-out Reset enabled)
+  CONFIG  CLKOUTEN = OFF        ; Clock Out Enable (CLKOUT function is disabled. I/O or oscillator function on the CLKOUT pin)
+  CONFIG  IESO = OFF            ; Internal/External Switchover (Internal/External Switchover mode is disabled)
+  CONFIG  FCMEN = OFF           ; Fail-Safe Clock Monitor Enable (Fail-Safe Clock Monitor is disabled)
+
+; CONFIG2
+  CONFIG  WRT = OFF             ; Flash Memory Self-Write Protection (Write protection off)
+  CONFIG  PLLEN = ON            ; PLL Enable (4x PLL enabled)
+  CONFIG  STVREN = OFF          ; Stack Overflow/Underflow Reset Enable (Stack Overflow or Underflow will cause a Reset)
+  CONFIG  BORV = LO             ; Brown-out Reset Voltage Selection (Brown-out Reset Voltage (Vbor), low trip point selected.)
+  CONFIG  DEBUG = OFF           ; In-Circuit Debugger Mode (In-Circuit Debugger disabled, ICSPCLK and ICSPDAT are general purpose I/O pins)
+  CONFIG  LVP = ON              ; Low-Voltage Programming Enable (Low-voltage programming enabled)
+
+// config statements should precede project file includes.
+#include <xc.inc>
+  
+
+PSECT resetVect, class=CODE, delta=2
+resetVect:
+    PAGESEL main
+    goto main
+    
+    
+PSECT code, delta=2
+main:
+    BANKSEL OSCCON ; move to the bank OSCCON is on (Bank1)
+    clrf OSCCON
+    movlw 0b01110011; this sets the oscillator to 8 MHz see page 65 to change
+    movwf OSCCON
+    BANKSEL PORTA ; move to the bank PORTA is on (Bank0)
+    clrf PORTA ; clear PORTA for good measure
+    BANKSEL LATA ; Move to the the bank LATA is on (bank 2)
+    clrf LATA ; clear LATA for digital I/O
+    BANKSEL ANSELA ; move to the bank ANSELA is on (bank3)
+    clrf ANSELA ; clear ANSEL for digital I/O
+    BANKSEL TRISA ; Move to the bank TRISA is on (bank 1) 
+    movlw 0b00111000 ; seting the RA0 - RA5 pins (0 output, 1 input, note not GPx)
+    movwf TRISA
+    
+    BANKSEL PR2 ; Set the PWM frequency (bank 0)
+    movlw 0b1100101; gives a 19.61 kHz PWM frequency (T = 51 us) when clock set to 8 MHz
+    movwf PR2
+		 
+    movlw 0b00000100; sets timer to on (bit 2) and prescaler to 1 (see page 197)
+    movwf T2CON
+        
+    BANKSEL CCP1CON ; Set the duty cycle information (bank 5)
+    movlw 0b00001100; bit 7-6 set to 00 means olny P1A active this is PIN2
+		    ; bit 5-4 are the lsb for the duty cycle setting
+		    ; bit 3-0 PWM mode Give the 00 in bits 7-6 the bits 1-0 could be xx
+    movwf CCP1CON
+    
+    movlw 0b00000100; Should set the pulse with to ~25 us (50% duty cycle)
+    movwf CCPR1L; 
+     
+    
+    
+mainloop:
+    nop
+    goto $-1
