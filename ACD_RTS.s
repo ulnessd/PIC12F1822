8a70650f82df18fa761fd81930cb810083839f84
Darin J. Ulness <120402820+ulnessd@users.noreply.github.com>
2025-11-16 17:12:12 -0600

    Create ACD_RTS.s
    Three in one. 
    Random Telegraph signal
    proximity detector
    touch detector.

---
+++ ACD_RTS.s
@@ -0,0 +1,115 @@
+
+; PIC12F1822 Configuration Bit Settings
+
+; Assembly source line config statements
+
+; CONFIG1
+  CONFIG  FOSC = INTOSC         ; Oscillator Selection (INTOSC oscillator: I/O function on CLKIN pin)
+  CONFIG  WDTE = OFF            ; Watchdog Timer Enable (WDT disabled)
+  CONFIG  PWRTE = OFF           ; Power-up Timer Enable (PWRT disabled)
+  CONFIG  MCLRE = OFF            ; MCLR Pin Function Select (MCLR/VPP pin function is MCLR)
+  CONFIG  CP = OFF              ; Flash Program Memory Code Protection (Program memory code protection is disabled)
+  CONFIG  CPD = OFF             ; Data Memory Code Protection (Data memory code protection is disabled)
+  CONFIG  BOREN = ON            ; Brown-out Reset Enable (Brown-out Reset enabled)
+  CONFIG  CLKOUTEN = OFF        ; Clock Out Enable (CLKOUT function is disabled. I/O or oscillator function on the CLKOUT pin)
+  CONFIG  IESO = OFF            ; Internal/External Switchover (Internal/External Switchover mode is disabled)
+  CONFIG  FCMEN = OFF           ; Fail-Safe Clock Monitor Enable (Fail-Safe Clock Monitor is disabled)
+
+; CONFIG2
+  CONFIG  WRT = OFF             ; Flash Memory Self-Write Protection (Write protection off)
+  CONFIG  PLLEN = ON            ; PLL Enable (4x PLL enabled)
+  CONFIG  STVREN = OFF          ; Stack Overflow/Underflow Reset Enable (Stack Overflow or Underflow will cause a Reset)
+  CONFIG  BORV = LO             ; Brown-out Reset Voltage Selection (Brown-out Reset Voltage (Vbor), low trip point selected.)
+  CONFIG  DEBUG = OFF           ; In-Circuit Debugger Mode (In-Circuit Debugger disabled, ICSPCLK and ICSPDAT are general purpose I/O pins)
+  CONFIG  LVP = ON              ; Low-Voltage Programming Enable (Low-voltage programming enabled)
+
+// config statements should precede project file includes.
+#include <xc.inc>
+  
+
+PSECT resetVect, class=CODE, delta=2
+resetVect:
+    PAGESEL main
+    goto main
+    
+    
+PSECT code, delta=2
+main:
+    BANKSEL OSCCON ; move to the bank OSCCON is on (Bank1)
+    clrf OSCCON
+    movlw 0b01111000; this sets the oscillator to 16 MHz see page 65 to change
+    movwf OSCCON
+    BANKSEL PORTA ; move to the bank PORTA is on (Bank0)
+    clrf PORTA ; clear PORTA for good measure
+    BANKSEL LATA ; Move to the the bank LATA is on (bank 2)
+    clrf LATA ; clear LATA for digital I/O
+    BANKSEL ANSELA ; move to the bank ANSELA is on (bank3)
+    movlw 0b00000001; sets RA0 to analog
+    movwf ANSELA
+
+    BANKSEL TRISA ; Move to the bank TRISA is on (bank 1) 
+    movlw 0b00111001 ; seting the RA0 - RA5 pins (0 output, 1 input, note not GPx)
+			; very counterintuitive but analog out needs to be set as input.
+    movwf TRISA
+    
+    ;extra stuff for ADC
+    BANKSEL ADCON0 ;this is bank 1
+    movlw 0b00000000	;this selects adcon0 setting. see page 130 for the settings
+    movwf ADCON0
+    
+    movlw 0b1000000
+    movwf ADCON1
+    
+    clrf ADRESH
+    clrf ADRESL
+
+
+    BANKSEL PORTA
+    bcf RA2
+
+mainloop:
+  
+    BANKSEL ADCON0
+    bsf ADCON0, 0 ; turning the ADC off
+    nop
+    bsf ADCON0, 1 ; turning ADC go on
+    
+    ; reading the 4 most significant bits in ADRESH
+    btfsc ADRESH, 3 
+    call ra2_toggle
+    nop
+    movlw 20  ; a momentary delay. It seems neccessary to have this slight delay
+    call delay
+    goto mainloop
+
+
+ra2_toggle:
+    BANKSEL PORTA
+    btfss RA2
+    movlw 0b00000000
+    btfsc RA2
+    movlw 0b00000001
+    btfss WREG, 0
+    bsf RA2
+    btfsc WREG, 0
+    bcf RA2
+    return
+    
+    
+delay: ; this is a three-layer nested loop. wouldn't need 3 but it is what I had 
+	; from other programs
+    movwf 0xB2
+out_out_loop:
+    movwf 0xB1
+outer_loop:
+    movwf 0xB0
+delay_loop:
+    decfsz 0xB0, 1
+    goto delay_loop
+    decfsz 0xB1, 1
+    goto outer_loop
+    decfsz 0xB2, 1
+    goto out_out_loop
+    retlw 0 ; the return sets the working register to zero. 
+    nop      
+ 
